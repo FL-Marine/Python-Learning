8281be2afc08d17ddf6748b5310b9c2415458b14
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2023-01-15 13:28:54 -0600

    Add files via upload

---
+++ Python for Non-Programmers/main.py
@@ -0,0 +1,36 @@
+# Variables hold information
+
+wallet = 41
+
+print(wallet)
+
+# Variables can change overtime
+
+wallet = 32 
+
+print(wallet)
+
+# Make a variable called day and set it to the date of the month
+day = 15
+print(day)
+
+# Types
+# int = whole number no decimals
+
+# float = numbers with decimals
+weight = 170.2
+
+# numbers can be positive or negative
+
+print(weight / 2)
+
+# Find something around in your life to represent an int and a float. Put them in variables
+
+car_value = 10000
+height_inches = 67.5
+
+print(car_value)
+print(height_inches)
+
+# Strings 
+
